5ecda971d2eb2a5a3d79787236c346cebdccc93a
LucasHS <poqueni50@gmail.com>
2024-11-18 15:16:32 +0100

    working new with numpy

--- neuron.py
+++ neuron.py
@@ -1,27 +1,14 @@
 inputs = [1, 2, 3, 2.5]
-weights_1 = [0.2, 0.8, -0.5, 1.0]
-weights_2 = [0.5, -0.91, 0.26, -0.5]
-weights_3 = [-0.26, -0.27, 0.17, 0.87]
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
+biass = [2, 3, 0.5]
 
-output = (
-    [inputs[0] * weights_1[0]
-    + inputs[1] * weights_1[1]
-    + inputs[2] * weights_1[2]
-    + inputs[3] * weights_1[3]
-    + bias1,
-    inputs[0] * weights_2[0]
-    + inputs[1] * weights_2[1]
-    + inputs[2] * weights_2[2]
-    + inputs[3] * weights_2[3]
-    + bias2,
-    inputs[0] * weights_3[0]
-    + inputs[1] * weights_3[1]
-    + inputs[2] * weights_3[2]
-    + inputs[3] * weights_3[3]
-    + bias3]
-)
 
-print(output)
+layer_outputs = []
+for neuron_weights, neuron_bias in zip(weights, biass):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input * weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
